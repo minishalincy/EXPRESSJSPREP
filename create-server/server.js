919eab12d277274eff89cc934078644dc9cf163a
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-07-28 10:00:21 +0530

    Creating server using Expressjs

--- create-server/server.js
+++ create-server/server.js
@@ -1,1 +1,17 @@
 const express = require("express")
+
+const app = express() //it acts as an object that provides tools
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to Home Page")
+})
+
+app.get('/about',(req,res)=>{
+    res.send("Contact us: +91 9479833344")
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server is running");
+    
+})
