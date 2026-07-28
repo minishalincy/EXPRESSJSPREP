9ddc122ae420061fdb679eddd064a386a01b0637
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-07-28 10:11:14 +0530

    res.json() response

--- create-server/server.js
+++ create-server/server.js
@@ -1,17 +1,24 @@
-const express = require("express")
+const express = require("express");
 
-const app = express() //it acts as an object that provides tools
+const app = express(); //it acts as an object that provides tools
 
-app.get('/',(req,res)=>{
-    res.send("Welcome to Home Page")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to Home Page");
+});
 
-app.get('/about',(req,res)=>{
-    res.send("Contact us: +91 9479833344")
-})
+app.get("/about", (req, res) => {
+  res.send("Contact us: +91 9479833344");
+});
 
+app.get("/student", (req, res) => {
+  let student = {
+    name: "Minisha",
+    city: "Bengaluru",
+  };
 
-app.listen(3000,()=>{
-    console.log("Server is running");
-    
-})
+  res.json(student);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running");
+});
