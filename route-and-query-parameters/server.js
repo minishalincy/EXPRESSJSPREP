0595971082ee9c0550a3e6b41d5db7fb4f6889d9
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-07-31 10:11:13 +0530

    query-parameter

--- route-and-query-parameters/server.js
+++ route-and-query-parameters/server.js
@@ -10,6 +10,11 @@ app.get("/student/:id", (req, res) => {
   res.send(`Student Id: ${req.params.id}`);
 });
 
+app.get("/student/", (req, res) => {
+    //query parameter
+  res.send(`Hola, ${req.query.name}`); //http://localhost:3000/student/?name=minisha
+});
+
 app.listen(3000, () => {
   console.log("server is running");
 });
